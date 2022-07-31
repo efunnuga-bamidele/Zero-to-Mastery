5a061b2a298659edb428138d52942fe7ea760941
Bamidele Efunnuga <bamideleefunnuga9@gmail.com>
2022-07-31 12:11:19 +0100

    Password Checker App

---
+++ passwordChecker.py
@@ -0,0 +1,6 @@
+print("This is a Password Checker Application")
+
+user_name = input("Enter a username: ")
+password = input("Enter a password: ")
+
+print(f"The password : {password} is {len(password)} characters long")
