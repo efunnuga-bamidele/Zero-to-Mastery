3ef1db18ed1699485a5f2b7a89a47a01cfec5b35
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-16 14:21:27 +0100

    feat:Implement Math function

---
+++ data-type/int_float_type.py
@@ -0,0 +1,15 @@
+# Integer Datatype
+
+print(type(2 + 4))
+print(2 - 4)
+print(2 * 4)
+print(type(2 / 4))
+
+print(2 ** 5) #Exponent
+print(9 // 4) #Double slash
+print(5 % 4) #Modular
+
+#math function
+
+print(round(3.9)) #Round off to nearest int
+print(abs(-20)) #absolute
