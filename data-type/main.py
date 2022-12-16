bd814f5b00f3cc65890f73f11bc418fe0f3b1234
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-16 14:09:45 +0100

    feat: Int and Float Data Type

---
+++ data-type/main.py
@@ -0,0 +1,18 @@
+# Functional Data Type
+
+    # int
+    # float
+    # Bool
+    # str
+    # list
+    # tuple
+    # set
+    # dict
+
+
+# Classes -> custom types
+
+# Specialized Data Types
+
+
+# None
