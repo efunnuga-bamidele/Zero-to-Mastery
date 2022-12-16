de2bfb0f97e289fb6f301892d81df4351b06ebd5
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-16 15:43:21 +0100

    feat: Builtin Functions

---
+++ variables/builtin_function.py
@@ -0,0 +1,10 @@
+greet = "Hellooo"
+print(greet[0:len(greet)])
+
+quote = 'to be or not to be'
+
+# String methods
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
