aaf5f82f622dd4003b270599434a64f7f8100808
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-16 14:33:04 +0100

    feat: Binary | Complex

--- data-type/operators.py
+++ data-type/operators.py
@@ -1,2 +1,15 @@
 #operator precedence
 print(20 - 3 * 4)
+# BODMAS
+
+# complex data type
+
+
+# Binary numbers
+
+# from num to binary
+print(bin(5))
+
+# from binary to number
+        # binary| base
+print(int('0b101', 2))
