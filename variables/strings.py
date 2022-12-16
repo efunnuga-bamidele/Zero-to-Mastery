2d735da2104f6dcd85c1063ea2242720584a8f4b
bamidele efunnuga <bamideleefunnuga9@gmal.com>
2022-12-16 15:12:42 +0100

    feat: String Formatting

---
+++ variables/strings.py
@@ -0,0 +1,10 @@
+#formatted strings
+
+name = 'Johnny'
+age = 55
+
+print("Hi "+ name +". You are "+ str(age) +' years old\n');
+
+print(f'Hi {name}. You are {age} years old\n')
+
+print('Hi {}. You are {} years old\n'.format(name, age))
