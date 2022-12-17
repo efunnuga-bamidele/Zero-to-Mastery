56b7aa5d257f197c09b80ad5f0430dbb6988d0b8
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-17 16:07:02 +0100

    feat: Completed List Functions

--- variables/list.py
+++ variables/list.py
@@ -32,6 +32,64 @@ print(new_cart)
 print(amazon_cart)
 
 
+# Matrix
+# Multi dimensional list
+matrix = [
+    [1,2,3],
+    [2,4,6],
+    [7,8,9]
+]
+
+print(matrix[0][2])
+
+# List Methods
+
+basket = [1,2,3,4,5]
+
+# adding method modifies original list
+basket.append(6) # add list to end of list
+basket.insert(5, 10) # collect position index and value to add to list
+basket.extend([7, 8, 9]) # add multiple values to list
+print(basket)
+
+#removing 
+basket.pop() #remove last value in the list
+basket.pop(0) # remove by index
+basket.remove(10) #remove the value located in the list
+# basket.clear() #remove the entire list values
+
+print(basket)
+
+alpha = [ 'a', 'b', 'c', 'd', 'e', 'd']
+
+print(alpha.index('d')) #returns the index of the value
+print(alpha.index('d', 0, 4)) #search for value between 0 - 4 index range and return index of value if found
+
+print('g' in alpha) # check if value exist in list and return True or False
+
+print(alpha.count('d')) # Return the number of times a value appears in a list
+alpha.sort() 
+alpha.reverse()
+print(alpha)
+print(len(alpha))
+
+print(alpha[::-1]) #use slicing to reverse a list but not modify the original list
+
+# print(list(range(1,100))) #generate a list between the range
+# print(list(range(101))) #generate a list between the range
+
+sentence = ' '.join(['Hi', 'my', 'name', 'is', 'Bamidele'])
+
+print(sentence)
+
+# list unpacking
+a, b, c, *other, d = [1,2,3,4,5,6,7,8,9]
+ 
+print(a)
+print(b)
+print(c)
+print(other)
+print(d)
 
 
 
