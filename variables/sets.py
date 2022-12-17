70e487bf2925ae25efec92c6cf7c4c86e1296b8a
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-17 17:03:53 +0100

    feat: sets and functions

---
+++ variables/sets.py
@@ -0,0 +1,27 @@
+#Sets
+# On ordered list of unique collection objects
+my_set = {1, 2, 3, 4, 5, 5}
+
+# my_set.add(100)
+# my_set.add(2)
+# print(my_set)
+
+my_list = [1, 2, 3, 4, 5, 5]
+
+my_converted_set = set(my_list)
+
+# print(my_converted_set)
+
+# print(1 in my_set)
+
+# print(list(my_converted_set))
+my_set = {4,5}
+your_set = {4,5,6,7,8,9,10}
+
+# print(my_set.difference(your_set))
+# print(my_set.difference_update(your_set))
+# print(my_set.discard(3))
+# print(my_set.intersection(your_set))
+# print(my_set.isdisjoint(your_set))
+# print(my_set.issubset(your_set))
+# print(your_set.issuperset(my_set))
