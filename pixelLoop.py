a20f2c57c5d9f325d37b598fd7e50aebc0449542
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-19 16:21:21 +0100

    feat: Simple GUI

---
+++ pixelLoop.py
@@ -0,0 +1,18 @@
+#Exercise!
+
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+for row in picture:
+    for pixel in row:
+        if (pixel == 1):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('') # need a new like after every row
