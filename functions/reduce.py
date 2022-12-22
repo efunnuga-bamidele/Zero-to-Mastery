0ae9fb18feb1c3e5113965aa042fd0830e7bfc60
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-22 14:34:04 +0100

    feat:Lambda Expression

--- functions/reduce.py
+++ functions/reduce.py
@@ -1,11 +1,22 @@
 # reduce function
 
+#lambda expression
+# Onetime anonimus function
+# lambda param: action(param) 
+
 from functools import reduce
 
+
 my_list = [1,2,3]
+
+def multiply_by2(item):
+    return item * 2
 
 def accumulator(acc, item):
     print(acc, item)
     return acc + item
 
 print(reduce(accumulator, my_list, 0))
+
+
+print(list(map(lambda item: item * 2, my_list)))
