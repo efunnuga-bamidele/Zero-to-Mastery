b63974a466e638a50533a670b181a6e211b7e178
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-23 16:03:47 +0100

    feat:Fabonacci Number Challange

---
+++ fibSeq.py
@@ -0,0 +1,15 @@
+# Fabonacci Number
+
+def fib(number):
+    a = 0
+    b = 1
+    for i in range(number):
+        yield a
+        temp = a
+        a = b
+        b = temp + b
+
+
+
+for x in fib(1000):
+    print(x)
