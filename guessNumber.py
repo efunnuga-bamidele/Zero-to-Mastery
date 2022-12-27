7a4beb8c2982f3221c7b1b9b3246b0de377ec369
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-12-27 15:58:50 +0100

    feat:Jokes Module

---
+++ guessNumber.py
@@ -0,0 +1,13 @@
+# import sys
+# from random import randint
+
+# print("Welcome to Guess a Number game!\n")
+
+# answer = randint(1, 10)
+
+# guess = input("Guess a number between [1 - 10]: ")
+# # number = sys.argv[1]
+
+# while True:
+
+#     try
---
+++ modules/example_two/main.py
@@ -0,0 +1,7 @@
+import random
+import sys
+
+# print(random.randint(1, 10))
+print(random.choice(list(range(100))))
+
+print(sys.argv)
